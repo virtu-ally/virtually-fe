f26afb2a2cf119dafb880c648b6f10fc1c05d05c
Kata Katona <katalap@gmail.com>
2025-04-15 23:09:50 +0100

    adding base url to vite config 2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/vitrtually-fe/",
+  base: "/virtually-fe/",
   plugins: [react(), tailwindcss()],
 });
