d10a9c040660a3f412c2adbb4efd2c75f32dc326
Kata Katona <katalap@gmail.com>
2025-05-04 19:11:23 +0100

    fixing routing for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,17 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/virtually-fe">
+    <HashRouter>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
