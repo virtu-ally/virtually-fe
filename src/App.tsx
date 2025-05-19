94cf0856de7594940fbf8d6a86591abd3c0a9c55
Kata Katona <katalap@gmail.com>
2025-05-19 23:34:43 +0100

    camcelcase issue sort

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,12 @@ import Logout from "./pages/Logout";
 import NewDashboard from "./pages/NewDashboard";
 import { PrivateRoute } from "./components/PrivateRoute";
 import Profile from "./pages/Profile";
-import { QuizProvider } from "./context/Quizcontext";
 import Template from "./pages/Template";
 import { ThemeProvider } from "./context/ThemeContext";
+
+// import { QuizProvider } from "./context/QuizContext";
+
+
 
 function App() {
   const location = useLocation();
