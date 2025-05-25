ed6345ec4ec121aa99413a125e71b9bf0a7bb3a1
Kata Katona <katalap@gmail.com>
2025-05-26 00:40:34 +0100

    url changes

--- src/api/getBaseUrl.ts
+++ src/api/getBaseUrl.ts
@@ -1,11 +1,10 @@
 export const getBaseUrl = () => {
   const isProd = import.meta.env.MODE === "production";
-  const prodUrl = import.meta.env.VITE_API_URL;
 
-  if (isProd && !prodUrl) {
-    console.error("Production API URL is not defined", prodUrl);
+  if (isProd) {
+    console.log("Production API URL");
     return "http://130.162.178.62:80";
   }
 
-  return isProd ? prodUrl : "http://localhost:8080";
+  return "http://localhost:8080";
 };
