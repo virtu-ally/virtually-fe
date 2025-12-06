7ab9614a0b6e631cd0dfb595bfddf398e465785a
Kata Katona <katalap@gmail.com>
2025-12-06 22:21:02 +0000

    changing to browserrouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,17 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/virtually-fe/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
